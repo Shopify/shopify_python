746420ae204d7434087752feec44158a1ded1386
Jason White <jason.white@shopify.com>
2017-03-01 09:50:36 -0500

    change license some more

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
+        'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 2',
