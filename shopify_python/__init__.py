bdad0eae14958ae3e1cda8c8f563e1c77ce80e64
Chris Fournier <chris.fournier@shopify.com>
2017-07-14 13:29:08 -0400

    Increase version number to 0.4.1

--- shopify_python/__init__.py
+++ shopify_python/__init__.py
@@ -7,7 +7,7 @@ from shopify_python import google_styleguide
 from shopify_python import shopify_styleguide
 
 
-__version__ = '0.4.0'
+__version__ = '0.4.1'
 
 
 def register(linter):  # type: (lint.PyLinter) -> None
