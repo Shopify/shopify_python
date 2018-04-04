831f936c01c232115d8e04a1e93f2a646ea76504
Alejandro Lujan Toro <alejandro.lujan@shopify.com>
2018-04-04 13:31:16 -0400

    Adding more tests for different uses of type annotations

--- tests/functional/sequence_of_string.py
+++ tests/functional/sequence_of_string.py
@@ -1,7 +1,16 @@
-# pylint:disable=missing-docstring,unused-import
+# pylint:disable=missing-docstring,unused-import,unused-argument
 import typing
 
 
-def some_method(seq):
-    # type: (typing.Sequence[str]) -> None # [sequence-of-string]
-    return seq
+def one_param(seq):
+    # type: (typing.Sequence[str]) -> str # [sequence-of-string]
+    copy = seq # type:typing.Sequence[str] # [sequence-of-string]
+    return copy[0]
+
+def multiple_params(message, seq, index):
+    # type: (string, typing.Sequence[str], int) -> str # [sequence-of-string]
+    return seq[index]
+
+def return_type(message_1, message_2):
+    # type: (str, str) -> typing.Sequence[str] # [sequence-of-string]
+    return [message_1, message_2]
