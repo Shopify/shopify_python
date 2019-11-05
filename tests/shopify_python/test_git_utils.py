afccc0f2c03f930bc9c7d7c5cd7339aa58be7329
Tamer Sherif <tamer.sherif@shopify.com>
2019-11-05 00:21:30 -0500

    adjusted test

--- tests/shopify_python/test_git_utils.py
+++ tests/shopify_python/test_git_utils.py
@@ -211,13 +211,13 @@ def test_dont_include_uncommited_or_untracked_files(main_repo, python_file):
     assert git_utils.changed_python_files_in_tree(main_repo.working_dir) == [os.path.basename(python_file)]
 
 
-def test_get_uncommitted_python_files(main_repo, python_file):
+def test_get_staged_python_files(main_repo, python_file):
     # type: (repo.Repo, str) -> None
 
     assert os.path.exists(os.path.join(main_repo.working_dir, os.path.basename(python_file)))
-    assert git_utils.uncommitted_python_files(main_repo.working_dir) == frozenset()
+    assert git_utils.staged_python_files(main_repo.working_dir) == frozenset()
     main_repo.index.add([python_file])
-    assert git_utils.uncommitted_python_files(main_repo.working_dir) == frozenset(['program.py'])
+    assert git_utils.staged_python_files(main_repo.working_dir) == frozenset(['program.py'])
 
 
 def test_dont_include_scripts_with_extensions(main_repo):
