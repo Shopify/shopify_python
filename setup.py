6b20c58c5c8d45c85bcb576270b7bd8800900f31
Patrick Begley <forge33@gmail.com>
2023-01-06 11:43:00 -0500

    Bumping gitpython for python3

--- setup.py
+++ setup.py
@@ -37,7 +37,8 @@ setuptools.setup(
     ],
     test_suite='tests',
     install_requires=[
-        'GitPython~=2.1.11',
+        'GitPython~=2.1.11 ; python_version<"3.0"',
+        'GitPython>=3.1.30 ; python_version>="3.0"',
         'typing~=3.6.6 ; python_version<"3.5"',
         'autopep8~=1.4',
     ],
